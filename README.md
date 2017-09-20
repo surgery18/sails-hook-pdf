13dbb4e945574dce42c2d1325450e83f112229f1
Scott Deutsch <surgery18@gmail.com>
2017-09-20 15:19:52 -0700

    init commit.

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # sails-hook-pdf
+
+[![Dependency Status](https://david-dm.org/surgery18/sails-hook-pdf.svg)](https://david-dm.org/surgery18/sails-hook-pdf)
+
+Pdf hook for [Sails JS](http://sailsjs.org), using [html-pdf](https://github.com/marcbachmann/node-html-pdf/README.md)
+
+### Installation
+
+`npm i --save sails-hook-pdf`
+
+### Usage
+
+`sails.hooks.email.pdf(template, data, options, cb)`
+
+Parameter      | Type                | Details
+-------------- | ------------------- |:---------------------------------
+template       | ((string))          | Relative path from `templateDir` (see "Configuration" below) to a folder containing email templates.
+data           | ((object))          | Data to use to replace template tokens
+options        | ((object))          | Email sending options (see [Nodemailer docs](https://github.com/andris9/Nodemailer/blob/v1.3.4/README.md#e-mail-message-fields))
+cb             | ((function))        | Callback to be run after the pdf is created (or if an error occurs). It will contain the path to the file.
+
+### Templates
+
+Templates are generated using your configured Sails [View Engine](http://sailsjs.org/#!/documentation/concepts/Views/ViewEngines.html), allowing for multiple template engines and layouts.  If Sails Views are disabled, will fallback to EJS templates. To define a new email template, create a new folder with the template name inside your `templateDir` directory, and add an **pdf.ejs** file inside the folder (substituting .ejs for your template engine).
+
+### Example
+
+Given the following **pdf.ejs** file contained in the folder **views/pdfTemplates/testPdf**:
+
+```
+<p>Hello <%=a%>!</p>
+```
+
+In your app you would run the following command.
+
+```
+sails.hooks.pdf.make(
+  "testPdf",
+  {
+    a: "Bob Dole",
+  },
+  {
+    output: 'assets/pdfs/mypdf.pdf'
+  },
+  function(err) {
+    console.log(err || "It worked!");
+  }
+);
+```
+
+will result in creating the pdf.
---
+++ index.js
@@ -0,0 +1,93 @@
+var ejs = require('ejs');
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+var _ = require('lodash');
+var pdf = require('html-pdf');
+
+
+module.exports = function PDF(sails){
+
+  var self;
+
+  var compileTemplate = function (view, data, cb) {
+    // Use Sails View Hook if available
+    if (sails.hooks.views && sails.hooks.views.render) {
+      var relPath = path.relative(sails.config.paths.views, view);
+      sails.hooks.views.render(relPath, data, cb);
+      return;
+    }
+
+    // No Sails View hook, fallback to ejs
+    fs.readFile(view + '.ejs', function (err, source) {
+      if (err) return cb(err);
+
+      try {
+        var compileFn = ejs.compile((source || "").toString(), {
+          cache: true, filename: view
+        });
+
+        cb(null, compileFn(data));
+      } catch (e) {
+        return cb(e);
+      }
+    });
+  };
+
+  return {
+
+    defaults: {
+      __configKey__: {
+        templateDir: path.resolve(sails.config.appPath, 'views/pdfTemplates'),
+      }
+    },
+
+    configure: function () {
+      // Ensure we have the full path, relative to app directory
+      sails.config[this.configKey].templateDir = path.resolve(sails.config.appPath, sails.config[this.configKey].templateDir);
+    },
+
+    initialize: function (cb) {
+      self = this;
+      return cb();
+    },
+
+    make: function (template, data, options, cb) {
+      data = data || {};
+      var templateDir = sails.config[self.configKey].templateDir;
+      var templatePath = path.join(templateDir, template);
+      var defaults = {
+        output: ""
+      };
+      var opt = _.defaults(options, defaults);
+
+      sails.log.verbose('Making PDF:', options);
+
+      async.auto({
+        compileHtmlTemplate: function (next) {
+          compileTemplate(templatePath + "/pdf", data, function (err, html) {
+            if (err) {
+              next(err);
+            } else {
+              next(null, html);
+            }
+          });
+        },
+
+        genPdf: ["compileHtmlTemplate", function(results, next) {
+          var html = results.compileHtmlTemplate;
+          pdf.create(html, opt).toFile(path.resolve(sails.config.appPath, opt.output), function(err, res) {
+            if (err) {
+              next(err);
+            } else {
+              console.log(res);
+              next(null, res);
+            }
+          });
+        }]
+
+      });
+    },
+
+  };
+};
